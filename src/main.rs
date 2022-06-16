87370ddf545e17292e64f0569ad5b790657ad1d5
Phydon <leann.phydon@gmail.com>
2022-06-16 08:26:20 +0200

    first stuff written

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::BTreeSet;
+
+struct NaturalNumber {
+    value: u64,
+    natural: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut set: BTreeSet<u64> = BTreeSet::new();
+
+    let zero = NaturalNumber {
+        value: 0,
+        natural: true,
+    };
+
+    let zero_string = "0".to_string();
+
+    add_to_set(&mut set, &zero_string);
+    println!("{:?}", &set);
+}
+
+fn add_to_set(set: &mut BTreeSet<u64>, num: &String) {
+    match num.parse::<u64>() {
+        Ok(_) => set.insert(num.parse::<u64>().unwrap() as u64),
+        Err(_) => false,
+    };
 }
